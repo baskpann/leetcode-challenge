97f5e83b813776f47d5d4a023e965509d6ea84bc
Baskaran Panneerselvam <baskpann@gmail.com>
2020-12-02 23:29:07 +0530

    Day 2:   Linked List Random Node

--- README.md
+++ README.md
@@ -22,3 +22,17 @@ Output: 2
 + Constraints:  
   * The number of nodes in the tree is in the range [0, 104].
   * -100 <= Node.val <= 100
+## Day 2
++ Problem link [link](https://leetcode.com/explore/challenge/card/december-leetcoding-challenge/569/week-1-december-1st-december-7th/3552/)
++ Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
++ Example:
+```
+// Init a singly linked list [1,2,3].
+ListNode head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+Solution solution = new Solution(head);
+
+// getRandom() should return either 1, 2, or 3 randomly. Each element should have equal probability of returning.
+solution.getRandom();
+```
---
+++ SolutionDay2.java
@@ -0,0 +1,30 @@
+// class ListNode {
+//          int val;
+//          ListNode next;
+//          ListNode() {}
+//          ListNode(int val) { this.val = val; }
+//          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+// }
+
+class Solution {
+
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    Random rand = null;
+    ListNode head = null;
+    public Solution(ListNode head) {
+        this.head = head;
+        this.rand = new Random();
+    }
+    
+    /** Returns a random node's value. */
+    public int getRandom() {
+        int res = -1;
+        ListNode tmp = head;
+        for(int i = 1; tmp != null; i++) {
+            if(rand.nextInt(i) == i - 1) res = tmp.val;
+            tmp = tmp.next;
+        }
+        return res;
+    }
+}
