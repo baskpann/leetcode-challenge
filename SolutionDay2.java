89282a34af5d70d4dde216a4bf875295489c1ba2
Baskaran Panneerselvam <baskpann@gmail.com>
2020-12-03 22:55:28 +0530

    Day 3: Increasing Order Search Tree

--- SolutionDay2.java
+++ SolutionDay2.java
@@ -1,11 +1,3 @@
-// class ListNode {
-//          int val;
-//          ListNode next;
-//          ListNode() {}
-//          ListNode(int val) { this.val = val; }
-//          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-// }
-
 class Solution {
 
     /** @param head The linked list's head.
