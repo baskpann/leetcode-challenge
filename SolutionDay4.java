f0950fb1ee46ebea5c98f9cf1eb6e0c2e3da2d6b
Baskaran Panneerselvam <baskpann@gmail.com>
2020-12-05 20:04:14 +0530

    Day 4: The kth Factor of n

--- SolutionDay4.java
+++ SolutionDay4.java
@@ -7,5 +7,20 @@ class Solution {
             if((n % i) == 0) res.add(i);
 
         return res.size() < k ? -1 : res.get(k - 1); 
+
+        /** 
+         * Alternate method
+         * 
+         * public int kthFactor(int n, int k) {
+         *  int countIter = 0;
+         *  for(int i = 1; i <= n; i++) {
+         *      if((n % i) == 0) {
+         *          countIter++;
+         *          if(countIter == k) return i;
+         *       }
+         *  }
+         *  return -1;
+         * }
+         */
     }
 }
